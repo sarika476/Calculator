fc0520d35d0fdd3659c7a6791952cd26e9d56149
sarika476 <17ce133@charusat.edu.in>
2022-04-08 12:22:22 +0530

    Added log files

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,26 +1,55 @@
+
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Calculator {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
     double getSqrt(int x)
     {
-        return Math.sqrt(x);
+        logger.info("[SQ ROOT] - " + x);
+        double result = Math.sqrt(x);
+        logger.info("[RESULT - SQ ROOT] - " + result);
+        return result;
+
     }
     long getFact(int x)
     {
+        logger.info("[FACTORIAL] - " + x);
         long fact=1;
         for(int i=x;i>0;i--)
         {
             fact=fact*i;
         }
+        logger.info("[RESULT - FACTORIAL] - " + fact);
         return fact;
     }
     double getLoge(int x)
     {
-        return Math.log(x);
+        logger.info("[NATURAL LOG] - " + x);
+        double result = 0;
+        try {
+
+            if (x <0 ) {
+                result = Double.NaN;
+                throw new ArithmeticException("Case of NaN 0.0/0.0");
+            }
+            else {
+                result = Math.log(x);
+            }
+        } catch (ArithmeticException error) {
+            System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
+        }
+        logger.info("[RESULT - NATURAL LOG] - " + result);
+        return result;
+
     }
     double getPow(int x,int y)
     {
-        return Math.pow(x,y);
+        logger.info("[POWER - " + x + " RAISED TO] " + y);
+        double result = Math.pow(x,y);
+        logger.info("[RESULT - POWER] - " + result);
+        return result;
     }
     public static void main(String args[])
     {
